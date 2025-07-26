94836ee393c26383c12ff529f275edba8eb2fade
Brandon G. Cobb <brandongrahamcobb@icloud.com>
2025-07-26 16:55:17 -0400

    482nd Vyrtuous

--- src/vyrtuous/sec/discord_oauth.py
+++ src/vyrtuous/sec/discord_oauth.py
@@ -42,7 +42,7 @@ class DiscordOAuth:
             f'?client_id={self.client_id}'
             f'&redirect_uri={self.redirect_uri}'
             f'&response_type=code'
-            f'&permissions=2141842833124'
+            f'&permissions=380385745984'
             f'&integration_type=0'
             f'&scope=bot'
         )
