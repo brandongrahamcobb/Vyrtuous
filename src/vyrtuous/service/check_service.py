859cda23696bed1d2c273b6711efaf5c4991baf5
Brandon G. Cobb <brandongrahamcobb@icloud.com>
2025-09-11 21:10:16 -0400

    561th Debug

--- src/vyrtuous/service/check_service.py
+++ src/vyrtuous/service/check_service.py
@@ -398,11 +398,11 @@ async def check_block(ctx: commands.Context, member: discord.Member, channel_id:
             target_roles.append("Developer")
     if ctx.guild:
         if member.id == ctx.author.id:
-            if "Owner" in author_roles:
-                return "Owner", True
-            return target_highest, False
+    if "Owner" in author_roles:
+        return "Owner", True
+    return target_highest, False
     if member.id == int(bot.config["discord_owner_id"]):
         target_roles.append("Owner")
     target_highest = max(target_roles, key=lambda r: role_hierarchy.index(r)) if target_roles else "Everyone"
-    success = role_hierarchy.index(target_highest) > role_hierarchy.index(author_highest)
+    success = role_hierarchy.index(target_highest) < role_hierarchy.index(author_highest)
     return target_highest, success
