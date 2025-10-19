1636499858dd8e3a6966130443116a2f32fb391a
Brandon G. Cobb <brandongrahamcobb@icloud.com>
2025-10-19 09:16:10 -0400

    666th Debug

--- src/vyrtuous/cogs/hybrid_commands.py
+++ src/vyrtuous/cogs/hybrid_commands.py
@@ -2854,7 +2854,7 @@ class Hybrid(commands.Cog):
             await self.handler.send_message(ctx, **kw)
         member_obj = await self.resolve_member(ctx, target)
         channel_obj = await self.resolve_channel(ctx, target)
-        if not member_obj or not target:
+        if not target:
             return await send(content=f'\U0001F6AB Could not resolve a valid channel or member from input: {target}.')
         is_owner_or_dev, is_mod_or_coord = await check_owner_dev_coord_mod(ctx, channel_obj)
         if not is_owner_or_dev and not is_mod_or_coord:
