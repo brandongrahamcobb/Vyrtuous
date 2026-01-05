f6d4860f5b3d4048680cd3ba1dd72e7e18a2b265
Brandon G. Cobb <brandongrahamcobb@icloud.com>
2026-01-05 14:48:20 -0500

    feat: test_coord

--- src/vyrtuous/tests/black_box/test_coord_command.py
+++ src/vyrtuous/tests/black_box/test_coord_command.py
@@ -24,37 +24,55 @@ import pytest
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    "command,channel_ref,member_ref",
+    "permission,command,ref_channel,ref_guild, ref_member,should_warn",
     [
-        ("coord {not_privileged_author_id} {voice_channel_one_id}", True, True),
-        ("coord {not_privileged_author_id} {voice_channel_one_id}", True, True)
-    ]
+        ("Administrator", "coord {not_privileged_author_id} {voice_channel_one_id}", True, False, True, False),
+        ("Administrator", "coord {not_privileged_author_id} {voice_channel_one_id}", True, False, True, False)
+    ],
+    indirect=['permission']
 )
-
-async def test_coord_command(bot, voice_channel_one, guild, privileged_author, not_privileged_author, prefix: Optional[str], role, command: Optional[str], channel_ref, member_ref):
-    administrator = Administrator(guild_snowflake=guild.id, member_snowflake=privileged_author.id, role_snowflakes=[role.id])
-    await administrator.grant()
-    try:
-        voice_channel_one.messages.clear()
-        formatted = command.format(
-            voice_channel_one_id=voice_channel_one.id,
-            not_privileged_author_id=not_privileged_author.id
-        )
-        captured = await prepared_command_handling(author=privileged_author, bot=bot, channel=voice_channel_one, cog="AdminCommands", content=formatted, guild=guild, isinstance_patch="vyrtuous.cogs.admin_commands.isinstance", prefix=prefix)
-        message = captured['message']
-        message_type = captured['type']
-        if isinstance(message, discord.Embed):
-            content = extract_embed_text(message)
-        elif isinstance(message, discord.File):
-            content = message.filename
-        else:
-            content = message
-        if message_type == "error":
-            print(f"{RED}Error:{RESET} {content}")
-        if message_type == "warning":
-            print(f"{YELLOW}Warning:{RESET} {content}")
-        if message_type == "success":
-            # print(f"{GREEN}Success:{RESET} {content}")
-            assert any(emoji in content for emoji in Emojis.EMOJIS)
-    finally:
-        await administrator.revoke()
+async def test_coord_command(
+    bot,
+    command: Optional[str],
+    not_privileged_author,
+    permission,
+    prefix: Optional[str],
+    privileged_author,
+    ref_channel,
+    ref_guild,
+    ref_member,
+    role,
+    should_warn,
+    text_channel,
+    voice_channel_one,guild
+):
+    channel_values = (voice_channel_one.mention, voice_channel_one.id)
+    guild_values = (guild.name, guild.id)
+    member_values = (not_privileged_author.mention, not_privileged_author.id)
+    formatted = command.format(
+        voice_channel_one_id=voice_channel_one.id,
+        not_privileged_author_id=not_privileged_author.id
+    )
+    captured = await prepared_command_handling(author=privileged_author, bot=bot, channel=text_channel, content=formatted, guild=guild, highest_role=permission, prefix=prefix)
+    message = captured[0]['message']
+    message_type = captured[0]['type']
+    if message.embeds:
+        embed = message.embeds[0]
+        content = extract_embed_text(embed)
+    elif message.embed:
+        content = extract_embed_text(message.embed)
+    else:
+        content = message.content
+    if message_type == "error":
+        print(f"{RED}Error:{RESET} {content}")
+    if message_type == "warning":
+        print(f"{YELLOW}Warning:{RESET} {content}")
+    if message_type == "success":
+        # print(f"{GREEN}Success:{RESET} {content}")
+        if ref_channel:
+            assert any(str(channel_value) in content for channel_value in channel_values)
+        if ref_guild:
+            assert any(str(guild_value) in content for guild_value in guild_values)
+        if ref_member:
+            assert any(str(member_value) in content for member_value in member_values)
+        assert any(emoji in content for emoji in Emojis.EMOJIS)
