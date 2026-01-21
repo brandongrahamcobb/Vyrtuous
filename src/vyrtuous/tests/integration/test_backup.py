c57de23e37c09f06fd92d154781dfeb90e0e6566
Brandon G. Cobb <brandongrahamcobb@icloud.com>
2026-01-21 10:39:10 -0500

    feat: backup test

---
+++ src/vyrtuous/tests/integration/test_backup.py
@@ -0,0 +1,48 @@
+"""test_backup.py The purpose of this program is to be the integration test for the backup list command for Vyrtuous.
+
+Copyright (C) 2025  https://github.com/brandongrahamcobb/Vyrtuous.git
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from typing import Optional
+
+import pytest
+
+from vyrtuous.tests.integration.test_suite import send_message
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "command",
+    [
+        ("!backup"),
+    ],
+)
+async def test_backup(bot, command: Optional[str]):
+    """
+    Backup the database 'vyrtuous'.
+
+    Parameters
+    ----------
+    None
+        No parameter required
+
+    Examples
+    --------
+    >>> !backup
+    [{emoji} {file}]
+    """
+    captured = await send_message(bot=bot, content=command)
+    assert captured
